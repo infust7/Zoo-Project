687dfea71ba7337bd9fe467f19e59285eee6aad6
Fscripter <61314160+Fscripter@users.noreply.github.com>
2023-03-05 20:35:18 -0500

    Animals added

---
+++ Model/Animals.java
@@ -0,0 +1,110 @@
+package Model;
+
+public class Animals {
+    private short age;
+    private float size;
+    private String name;
+    private String species;
+    private boolean dangerous;
+    private String psychological;
+    private String acquisitionMethod;
+    private short foodFrequencyPerDay;
+    private String allergies;
+    private String feeding;
+
+    public Animals(short age, float size, String name, String species, boolean dangerous, String psychological,
+            String acquisitionMethod, short foodFrequencyPerDay, String allergies, String feeding) {
+        this.age = age;
+        this.size = size;
+        this.name = name;
+        this.species = species;
+        this.dangerous = dangerous;
+        this.psychological = psychological;
+        this.acquisitionMethod = acquisitionMethod;
+        this.foodFrequencyPerDay = foodFrequencyPerDay;
+        this.allergies = allergies;
+        this.feeding = feeding;
+    }
+
+    public short getAge() {
+        return age;
+    }
+
+    public void setAge(short age) {
+        this.age = age;
+    }
+
+    public float getSize() {
+        return size;
+    }
+
+    public void setSize(float size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public boolean isDangerous() {
+        return dangerous;
+    }
+
+    public void setDangerous(boolean dangerous) {
+        this.dangerous = dangerous;
+    }
+
+    public String getPsychological() {
+        return psychological;
+    }
+
+    public void setPsychological(String psychological) {
+        this.psychological = psychological;
+    }
+
+    public String getAcquisitionMethod() {
+        return acquisitionMethod;
+    }
+
+    public void setAcquisitionMethod(String acquisitionMethod) {
+        this.acquisitionMethod = acquisitionMethod;
+    }
+
+    public short getFoodFrequencyPerDay() {
+        return foodFrequencyPerDay;
+    }
+
+    public void setFoodFrequencyPerDay(short foodFrequencyPerDay) {
+        this.foodFrequencyPerDay = foodFrequencyPerDay;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public String getFeeding() {
+        return feeding;
+    }
+
+    public void setFeeding(String feeding) {
+        this.feeding = feeding;
+    }
+
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Zoo-Project
+
+###
+
+Zoo register for programming techniques, includes architecture MVC
+
+###
+
+- Model
+  - Animals ( Made by _Fscripter_)
+  - Users ( in progression )
+
+_Universidad de Antioquia_
